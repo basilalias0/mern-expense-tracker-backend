a01d738c8deced0f0abbfe40807d451c034575b1
basilalias0 <basilalyas2000@gmail.com>
2024-05-22 13:30:01 +0530

    PORT changed 1

--- index.js
+++ index.js
@@ -47,6 +47,6 @@ app.use('/api/v1/transaction',transactionRouter)
 app.use(errorHandler)
 
 
-app.listen(5000,()=>{
-    console.log("http://localhost:5000");
+app.listen(3000,()=>{
+    console.log("http://localhost:3000");
 })
